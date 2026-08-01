3a197cd4994854ce1d087c262c782a01179be33e
Saurabh <saurabh.b0902@gmail.com>
2026-08-01 10:42:26 +0530

    Week1 complete: CLI workbench with 4 AI tasks

---
+++ myProg1.py
@@ -0,0 +1,22 @@
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="openai/gpt-oss-120b",
+)
+
+# print(response.choices[0].message.content)
+print (response)
